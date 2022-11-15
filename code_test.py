5de24a9f3ac9ec67bede638a6ea565ea4d1e6505
SpeagleYao <speagleyao@sjtu.edu.cn>
2022-11-15 18:49:07 +0800

    speck Day 2

--- code_test.py
+++ code_test.py
@@ -3,19 +3,35 @@
 import numpy as np
 from os import urandom
 
-def WORD_SIZE():
-    return 16
+# def WORD_SIZE():
+#     return 16
 
-MASK_VAL = 2 ** WORD_SIZE() - 1
+# MASK_VAL = 2 ** WORD_SIZE() - 1
 
-def rol(x, k):
-    return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)))
+# def rol(x, k):
+#     return(((x << k) & MASK_VAL) | (x >> (WORD_SIZE() - k)))
 
-def rol_um(x, k):
-    return(((x << k)) | (x >> (WORD_SIZE() - k)))
+# def rol_um(x, k):
+#     return((x << k)) | (x >> (WORD_SIZE() - k))
 
-x = np.frombuffer(urandom(2*10),dtype=np.uint16)
+# x = np.frombuffer(urandom(2*10),dtype=np.uint16)
+# print(x)
+# x = urandom(1)
+# print(x)
+# x = np.frombuffer(x,dtype=np.uint16)
+# print(x)
+# print(rol(x, 4))
+# print(rol_um(x, 4))
+# print((x+2**20)&MASK_VAL)
 
-print(x)
-print(rol(x, 4))
-print(rol_um(x, 4))
+# k = np.frombuffer(urandom(8*5), dtype=np.uint16).reshape(4, -1)
+# print(k.shape)
+# print(k)
+# print(k[3])
+# k = list(reversed(k))
+# print(k[0])
+# print(k[1:])
+
+n = 10
+x = np.frombuffer(urandom(2*n),dtype=np.uint16)
+print(x.shape[0])
--- crypto/speck.py
+++ crypto/speck.py
@@ -1,4 +1,5 @@
 import numpy as np
+from os import urandom
 
 class speck():
 
@@ -8,29 +9,51 @@ class speck():
         self.block_size = block_size
         self.word_size = block_size/2
         self.key_size = key_size
+        self.mask = 2 ** key_size - 1
 
-    # Rotation to left
-    def rol(x, t):
-        x << t
-        return ()
+    # Left Rotation
+    def rol(self, x, t):
+        return ((x << t) & self.mask) | (x >> (self.word_size - t))
 
-    # Rotation to right
-    def ror():
-        pass
+    # Right Rotation
+    def ror(self, x, t):
+        return (x >> t) | ((x << (self.word_size - t)) & self.mask)
 
+    # urandom() returns one bytes, which is 8 bits of true random number
+    # As we need uint16 here, we need two bytes for one data
+    def rand_uint16(self, n):
+        return np.frombuffer(urandom(2 * n), dtype = np.uint16)
+    
     # Generate keys for nr rounds
-    def expand_key():
-        pass
+    # This is a 4-word key schedules
+    # Thus input k: (4, batch_size)
+    def expand_key(self, k, nr):
+        ks = [0 for i in range(nr)]
+        k = list(reversed(k))
+        ks[0] = k[0]
+        l = k[1:]
+        for i in range(nr-1):
+            l[i%3], ks[i+1] = self.enc_one_round((l[i%3], ks[i]), i)
+        return ks
 
     # Enc for one round
-    def enc_one_round():
-        pass
+    def enc_one_round(self, p, k):
+        x, y = p[0], p[1]
+        x = ((self.ror(x, self.alpha) + y) & self.mask) ^ k
+        y = self.rol(y, self.beta) ^ x
+        return x, y
 
     # Multi-rounds enc using enc_one_round
     # Input p would be array of plaintext_l and plaintext r, each with 16 bits
+    # For batch, p: ((batch_size), (batch_size))
     # nr represents encryption rounds
-    def encrypt(p, nr):
-        pass
+    def encrypt(self, p, nr):
+        x, y = p[0], p[1]
+        keys = self.rand_uint16(4 * p[0].shape[0]).reshape(4, -1)
+        ks = self.expand_key(keys, nr)
+        for k in ks:
+            x, y = self.enc_one_round((x, y), k)
+        return (x, y)
 
     # Dec for one round
     def dec_one_round():
