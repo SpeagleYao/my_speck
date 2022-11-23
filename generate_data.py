f3fca5da3ac67a7b8c65cab0430bc3eba339175b
SpeagleYao <speagleyao@sjtu.edu.cn>
2022-11-23 18:59:39 +0800

    new generate data

--- generate_data.py
+++ generate_data.py
@@ -1,14 +1,16 @@
 from crypto.speck import speck
 import numpy as np
+import os
 
 NUM_TRAIN_PAIRS = 10**7
 NUM_TEST_PAIRS = 10**5
-ROUNDS = 5
+ROUNDS = 7
 
 speck = speck()
 X, Y = speck.generate_train_data(NUM_TRAIN_PAIRS, ROUNDS)
-np.save("./data/train_data_"+str(ROUNDS)+"r.npy", X)
-np.save("./data/train_label_"+str(ROUNDS)+"r.npy", Y)
+if not os.path.exists("./data/"+str(ROUNDS)+"r/"): os.makedirs("./data/"+str(ROUNDS)+"r/")
+np.save("./data/"+str(ROUNDS)+"r/train_data_"+str(ROUNDS)+"r.npy", X)
+np.save("./data/"+str(ROUNDS)+"r/train_label_"+str(ROUNDS)+"r.npy", Y)
 X, Y = speck.generate_train_data(NUM_TEST_PAIRS, ROUNDS)
-np.save("./data/test_data_"+str(ROUNDS)+"r.npy", X)
-np.save("./data/test_label_"+str(ROUNDS)+"r.npy", Y)
+np.save("./data/"+str(ROUNDS)+"r/test_data_"+str(ROUNDS)+"r.npy", X)
+np.save("./data/"+str(ROUNDS)+"r/test_label_"+str(ROUNDS)+"r.npy", Y)
